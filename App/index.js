1217061bb8c16fca55cd090000a197f0219aa2b7
Mohammed Ahmed <mahme029@gold.ac.uk>
2024-02-19 13:15:37 +0000

    Dependencies installed

---
+++ App/index.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const Home = () => {
+    return (
+        <View>
+            <Text>Wagwaan Taliban</Text>
+        </View>
+    )
+}
+
+export default Home;
